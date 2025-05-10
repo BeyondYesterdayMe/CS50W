0759ff31caab1cee9ac8406e4b5e080f0c7d404d
Seongsim Ha <beyondyesterdayme@gmail.com>
2025-05-10 14:09:55 +0900

    [project1] Compliment Search page

--- web50/projects/2020/x/wiki/encyclopedia/views.py
+++ web50/projects/2020/x/wiki/encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
@@ -10,11 +12,32 @@ def index(request):
     })
 
 def entry(request, title):
-    entries = util.get_entry(title)
-    if not entries:
+    entry = util.get_entry(title)
+    if not entry:
         return HttpResponseNotFound()
 
     return render(request, "encyclopedia/entry.html" , {
         "title" : title,
-        "entries" : util.get_entry(title) 
+        "entry" : entry 
     })
+
+def search(request):
+    query = request.GET.get("q")
+    #print(f"query: {query}, Type: {type(query)}")
+    entry = util.get_entry(query)
+
+    if not entry :
+        # 정확히 일치하는 게 없다면, substring이 일치하는 목록 보여주기
+        results = []
+        for filename in util.list_entries():
+            #print(f"filename: {filename}")
+            if query.lower() in filename.lower():
+                results.append(filename)
+
+        return render(request, "encyclopedia/search.html", {
+            "results" : results
+        })
+        
+    else:
+        # 정확히 일치하는게 있으면 그 entry로 redirect
+        return HttpResponseRedirect(reverse("entry", args=[query]))
